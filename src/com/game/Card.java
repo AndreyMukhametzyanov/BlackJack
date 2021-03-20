1b74546dbc4af40918dd098f54c4d5d398458a6e
AndreyMukhametzyanov <atixan12@gmail.com>
2021-03-20 15:33:41 +0400

    Add shuffle method

--- src/com/game/Card.java
+++ src/com/game/Card.java
@@ -1,14 +1,12 @@
 package com.game;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Card {
     private final static String[] numbersOfCard = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}; // номер карты B
     private final static String[] suits = {"♠", "♥", "♦", "♣"}; //масть пики червы крести буби в unicode
     private final static Map<String, Integer> rankMap = new HashMap<>();
-
     {
         rankMap.put("2", 2);
         rankMap.put("3", 3);
@@ -27,22 +25,23 @@ public class Card {
 
 
     private final String numberOfCard; //для конкретной карты
-    private final String suit;
+    private final String suit; // для конкретной масти
 
-    public Card(String numberOfCard, String suit) {
+    public Card(String numberOfCard, String suit) { // конструтор класса Card
         this.numberOfCard = numberOfCard;
         this.suit = suit;
     }
 
     public String getNumberOfCard() {
         return numberOfCard;
-    }
-
+    } // получение конкретной карты
     public String getSuit() {
         return suit;
-    }
+    } // получение конкретной масти
 
-    public static Integer rankOfCard(Card card) {
+
+
+    public static Integer rankOfCard(Card card) { //
         String number = card.getNumberOfCard();
         return rankMap.get(number);
     }
@@ -50,24 +49,29 @@ public class Card {
     @Override
     public String toString() {
         return String.format("[%s%s]", numberOfCard, suit);
-    }
+    } //переопределяем метод для вывода
 
 
-    public static ArrayList<Card> deck () {// создание колоды
-        ArrayList<Card> result = new ArrayList<>(numbersOfCard.length * suits.length);
+    public static List<Card> deck () {// создание колоды
+        List<Card> result = new ArrayList<>(numbersOfCard.length * suits.length);
         for (String suit : suits) {
             for (String number : numbersOfCard) {
                 result.add(new Card(number, suit));
             }
-
         }
         return result;
 
     }
-    public static Integer pointsCount (ArrayList<Card> cards){
+    public static List<Card> shuffle (List<Card> deck){
+        List<Card> shuffleDeck = new ArrayList<>(deck);
+        Collections.shuffle(shuffleDeck);
+        return shuffleDeck;
+    }
+    public static Integer pointsCount (ArrayList<Card> cards){ // метод для подсчета очков
 
         return 0;
     }
+
 
 
 
--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Card card = new Card("2", "♠");
         var deck = Card.deck();
-        System.out.println(deck.size());
+        System.out.println("Исходная колода карт" + deck);
+        System.out.println("Количество карт в колоде "+ deck.size() + "шт.");
+        var deckShuffle = Card.shuffle(Card.deck());
+        System.out.println("Перетасованная колода карт "+ deckShuffle);
     }
 }
